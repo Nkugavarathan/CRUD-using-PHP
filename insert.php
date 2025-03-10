26e19b9ecfae51ab031e3c315450191bd84f4852
Nkugavarathan <kugavarathan28@gmail.com>
2025-03-10 17:32:27 +0530

    reset button setuped

--- insert.php
+++ insert.php
@@ -53,7 +53,7 @@ do {
         }
         ?>
 
-        <form action="insert.php" method="post">
+        <form action="insert.php" method="post" id="clientForm">
             <div class="row mb-3">
                 <label for="" class="col-sm-3 col-form-label">Name</label>
                 <div class="col-sm-6">
@@ -97,17 +97,21 @@ do {
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a type="submit" class="btn btn-outline-primary">Reset</a>
+                    <button type="button" class="btn btn-warning" onclick="resetForm()">Reset</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a type="submit" class="btn btn-outline-primary" href="./index.php">Cancel</a>
+                    <a type="submit" class="btn btn-danger" href="./index.php">Cancel</a>
                 </div>
 
             </div>
         </form>
     </div>
 
-
+    <script>
+        function resetForm() {
+            document.getElementById("clientForm").reset();
+        }
+    </script>
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"> </script>
 
 </body>
